a7795df333efaa38d3c54585455cfe731599d433
John Edwards <edwards1860@gmail.com>
2025-07-17 18:08:15 -0700

    add dictionaries

---
+++ data-raw/build_data_dictionaries.R
@@ -0,0 +1,15 @@
+library(usethis)
+library(data.table)
+library(dplyr)
+library(stringr)
+
+lapply(
+  list.files("data-raw", pattern = ".csv", full.names = TRUE),
+  \(x) {
+    file <- fread(x) |>
+      mutate_all(str_squish)
+    name <- gsub(".csv", "", gsub("data-raw/", "", x))
+    assign(name, file)
+    do.call("use_data", list(as.name(name), overwrite = TRUE))
+  }
+)
